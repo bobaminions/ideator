4a9958321fb5f854e48cc94bef4263981d437c31
Ramesh Thakulla <bobaminions@gmail.com>
2016-12-09 19:16:30 +0000

    Add CRUD methods in ideator app

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,8 +7,40 @@ class IdeasController < ApplicationController
     #Method to create new Idea
     def create
         @idea = Idea.create(idea_params)
+        if @idea.valid?
+            #
+        else
+            #
+        end
+        
         redirect_to root_path
     end
+    #end Idea create method
+    
+    #Method to edit ideas
+    def edit
+        @idea = Idea.find(params[:id])
+    end
+    #end Idea edit method
+    
+    #Method to update ideas
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(idea_params)
+            redirect_to root_path
+        else
+            redirect_to edit_idea_path(params[:id])
+        end
+    end
+    #end Idea update method
+    
+    #Method to delete ideas
+    def destroy
+        @idea = Idea.find(params[:id])
+        @idea.destroy
+        redirect_to root_path
+    end
+    #end Idea delete method
     
     #This method prevents users from passing malicious code to the server
     private
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,5 @@
+#Creating Idea Model
 class Idea < ActiveRecord::Base
+    #Form validation for Ideas
+    validates :description, :author, presence: true
 end
